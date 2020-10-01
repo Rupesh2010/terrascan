72cc38e20e79d55759b4d679e4e62e52ea0e00e7
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-10-01 14:56:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,9 @@ module "sg_default" {
   sg_name = "test"
   sg_description = "Security Group managed by Terraform"
   vpc_id = "vpc-26e8dc5c"
-  
-  ### Inbound rules ###
-  inbound_rules = {
-    "0" = [ "0.0.0.0/0", "5", "5", "TCP" ]
-    "1" = [ "0.0.0.0/0", "443", "443", "TCP" ]
-    "2" = [ "0.0.0.0/0", "1234", "4321", "UDP" ]
-  }
-  
-  ### Outbound rules ###  
-  outbound_rules = {
-    "0" = [ "0.0.0.0/0", "0", "0", "-1" ]
-  }
-
-  ### Tags ###  
+  inbound_rules = "80"
+  outbound_rules = "80"
+  cidr_blockd = "0.0.0.0/0"
   tags = {
     Name = "test"
     Project = "test"
