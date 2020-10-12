19d769ad7c9441c9fe11c60f5863782dd470f187
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-10-12 14:16:28 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  alias  = "should-fail"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  access_key = "test"
+  secret_key = "test"
 }
